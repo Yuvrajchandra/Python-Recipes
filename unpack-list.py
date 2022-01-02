df285308a75252fec072ca3704e035a06d818806
YuvrajChandra <Singhyuvraj179@gmail.com>
2022-01-03 00:59:38 +0530

    Added a recipe

---
+++ unpack-list.py
@@ -0,0 +1,6 @@
+list = ["Hey", 23, 0.34, (55, 76)]
+a, b, c, d = list
+print(a)
+print(b)
+print(c)
+print(d)
---
+++ unpack-tuple.py
@@ -0,0 +1,7 @@
+tup = (12, 23, 34, 45, 56)
+a, b, c, d, e = tup
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
