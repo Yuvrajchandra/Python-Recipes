ef443cc0344a65208debcd11edb71fab8d86e756
YuvrajChandra <Singhyuvraj179@gmail.com>
2022-03-19 18:42:13 +0530

    Added a recipe

---
+++ returning-multiple-values-list.py
@@ -0,0 +1,8 @@
+def returnMultipleLanguages():
+    language1 = "English"
+    language2 = "Hindi"
+    language3 = "French"
+    return [language1, language2, language3]
+
+languages = returnMultipleLanguages()
+print(languages)
---
+++ returning-multiple-values-tuple.py
@@ -0,0 +1,8 @@
+def returnMultipleSports():
+    sport1 = "football"
+    sport2 = "cricket"
+    sport3 = "basketball"
+    return sport1, sport2, sport3
+
+sports = returnMultipleSports()
+print(sports)
