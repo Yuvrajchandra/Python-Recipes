b25cc3afc7f97ef4c0655184fa337e6e08ce3e6a
YuvrajChandra <Singhyuvraj179@gmail.com>
2022-03-19 19:19:20 +0530

    Added a recipe

---
+++ reverse-iterate-using-range.py
@@ -0,0 +1,3 @@
+list1 = [1, 2, 3, 4, 5, 6, 7]
+for i in range(len(list1)-1, -1, -1):
+    print(list1[i])
---
+++ reverse-iterate-using-reversed.py
@@ -0,0 +1,3 @@
+list1 = [1, 2, 3, 4, 5, 6, 7]
+for elem in reversed(list1):
+    print(elem)
